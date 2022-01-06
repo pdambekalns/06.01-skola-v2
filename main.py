f57b649c1b1f5fbc6a08188d5eee9812885a813b
pdambekalns <97154866+pdambekalns@users.noreply.github.com>
2022-01-06 07:32:28 +0000

    pilnveidots uzdevums

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import mod
-paterins=float(input("Ievadi savu elektrības patēriņu kilovatstundās! "))
+paterinsPirms=float(input("Ievadi savu iepriekšējo elektrības skaitītāja rādījumu kilovatstundās! "))
+paterinsPec=float(input("ievadi savu esošo elektrības skaitītāja rādījumu kilovatstundās! "))
+paterins=paterinsPec-paterinsPirms
 ligums=float(input("Ievadi līguma garumu gados"))
 print("Par elektrību jāmaksā", mod.maksa(paterins,ligums),"eiro")
